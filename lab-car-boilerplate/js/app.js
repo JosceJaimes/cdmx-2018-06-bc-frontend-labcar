c87f5fd7d26aa757487b4b4773aefa9015b5669b
Josce Jaimes <nanayhachi94@gmail>
2018-08-27 12:58:25 -0500

    Subiendo segunda version de maquetacion mobile first

--- lab-car-boilerplate/js/app.js
+++ lab-car-boilerplate/js/app.js
@@ -1,1 +1,21 @@
 // La funcionalidad de tu proyecto
+
+let btnNavigaitor = document.getElementById('btn2')
+let btnconductor = document.getElementById('btn1')
+
+
+// findMe = () =>{
+//     alert("hol");
+// //     let output = document.getElementById('maps')
+// // if( navigator.geolocation){
+// //     output.innerHTML = "<p>Tu navegador soporta Geolocalizacion</p>";
+
+// // }else{
+// //     output.innerHTML = "<p>Tu navegador  no soporta Geolocalizacion</p>";
+// // }
+// };
+
+btnconductor.addEventListener('click', e =>{
+console.log("hol");
+
+});
